6ee19bf34b0623b78661c655763a7de2c8fee190
Tyler <tythedeveloper10@gmail.com>
2024-05-14 06:53:18 -0500

    adjust page to describe skills

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -36,67 +36,56 @@ function Tool({
 
 export const metadata = {
   title: 'Uses',
-  description: 'Software I use, gadgets I love, and other things I recommend.',
+  description: 'Software technologies I have experience in.',
 }
 
-export default function Uses() {
+export default function Skills() {
   return (
     <SimpleLayout
-      title="Software I use, gadgets I love, and other things I recommend."
-      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      title="Software technologies I have experience in."
+      intro="These are tools and technologies I have a great deal of experience in and have used for both professional and personal projects."
     >
       <div className="space-y-20">
-        <ToolsSection title="Workstation">
-          <Tool title="16” MacBook Pro, M1 Max, 64GB RAM (2021)">
-            I was using an Intel-based 16” MacBook Pro prior to this and the
-            difference is night and day. I’ve never heard the fans turn on a
-            single time, even under the incredibly heavy loads I put it through
-            with our various launch simulations.
+        <ToolsSection title="UI and State Management">
+          <Tool title="React">
+            Well versed in functional component design, use of standard and
+            custom hooks
           </Tool>
-          <Tool title="Apple Pro Display XDR (Standard Glass)">
+          <Tool title="Redux">
             The only display on the market if you want something HiDPI and
             bigger than 27”. When you’re working at planetary scale, every pixel
             you can get counts.
           </Tool>
-          <Tool title="IBM Model M SSK Industrial Keyboard">
+          <Tool title="Typescript">
             They don’t make keyboards the way they used to. I buy these any time
             I see them go up for sale and keep them in storage in case I need
             parts or need to retire my main.
           </Tool>
-          <Tool title="Apple Magic Trackpad">
-            Something about all the gestures makes me feel like a wizard with
-            special powers. I really like feeling like a wizard with special
-            powers.
-          </Tool>
-          <Tool title="Herman Miller Aeron Chair">
-            If I’m going to slouch in the worst ergonomic position imaginable
-            all day, I might as well do it in an expensive chair.
-          </Tool>
         </ToolsSection>
-        <ToolsSection title="Development tools">
-          <Tool title="Sublime Text 4">
+        <ToolsSection title="Styling">
+          <Tool title="TailwindCSS">
             I don’t care if it’s missing all of the fancy IDE features everyone
             else relies on, Sublime Text is still the best text editor ever
             made.
           </Tool>
-          <Tool title="iTerm2">
+          <Tool title="SCSS">
             I’m honestly not even sure what features I get with this that aren’t
             just part of the macOS Terminal but it’s what I use.
           </Tool>
-          <Tool title="TablePlus">
+          <Tool title="CSS Modules">
             Great software for working with databases. Has saved me from
             building about a thousand admin interfaces for my various projects
             over the years.
           </Tool>
         </ToolsSection>
-        <ToolsSection title="Design">
-          <Tool title="Figma">
+        <ToolsSection title="Server Side">
+          <Tool title=".NET (inclusive of full framework and .NET 6)">
             We started using Figma as just a design tool but now it’s become our
             virtual whiteboard for the entire company. Never would have expected
             the collaboration features to be the real hook.
           </Tool>
         </ToolsSection>
-        <ToolsSection title="Productivity">
+        <ToolsSection title="Databases">
           <Tool title="Alfred">
             It’s not the newest kid on the block but it’s still the fastest. The
             Sublime Text of the application launcher world.
@@ -115,6 +104,19 @@ export default function Uses() {
           <Tool title="Focus">
             Simple tool for blocking distracting websites when I need to just do
             the work and get some momentum going.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Cloud">
+          <Tool title="AWS Lambda">
+            It’s not the newest kid on the block but it’s still the fastest. The
+            Sublime Text of the application launcher world.
+          </Tool>
+          <Tool title="CloudFormation">
+            Using a daily notes system instead of trying to keep things
+            organized by topics has been super powerful for me. And with
+            Reflect, it’s still easy for me to keep all of that stuff
+            discoverable by topic even though all of my writing happens in the
+            daily note.
           </Tool>
         </ToolsSection>
       </div>
